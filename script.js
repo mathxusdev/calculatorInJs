aed413d0632e35809e4c2a17d859c3c35cf1c475
mathxusdev <seilaboyyy@gmail.com>
2024-12-16 01:54:05 -0300

    Complete

---
+++ script.js
@@ -0,0 +1,27 @@
+function sum() {
+    let a = +prompt('Number: ', 0);
+    let b = +prompt('Number: ', 0);
+    alert(a + b);
+    return a + b;
+}
+
+function sub() {
+    let a = +prompt('Number: ', 0);
+    let b = +prompt('Number: ', 0);
+    alert(a - b);
+    return a - b;
+}
+
+function mul() {
+    let a = +prompt('Number: ', 0);
+    let b = +prompt('Number: ', 0);
+    alert(a * b);
+    return a * b;
+}
+
+function div() {
+    let a = +prompt('Number: ', 0);
+    let b = +prompt('Number: ', 0);
+    alert(a / b);
+    return a / b;
+}
