35cd7f55368758e7c232ead52c8ee2390230a6e5
mathxusdev <seilaboyyy@gmail.com>
2024-12-18 01:26:11 -0300

    New button 'rest of division' or 'rod' added

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ function div() {
     alert(a / b);
     return a / b;
 }
+
+function restOfDiv() {
+    let a = +prompt('Number: ', 0);
+    let b = +prompt('Number: ', 0);
+    alert(a % b);
+    return a % b;
+}
